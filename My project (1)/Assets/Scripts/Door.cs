d33a1e9d8e25b6214eb9e1d12ffcf032e397c6f4
Erickrapha <erick.r@escolar.ifrn.edu.br>
2025-05-14 11:21:48 -0300

    Desastre

---
+++ My project (1)/Assets/Scripts/Door.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool trigger;
+    public BoxCollider2D boxCollider;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //OnTriggerEnter2D();
+    }
+}
