ca96b1695c0ae89f18d91f990a011f46785e35c4
Erickrapha <erick.r@escolar.ifrn.edu.br>
2025-07-11 10:55:25 -0300

    Saida

--- My project (1)/Assets/Scripts/Refrigerante/inserir.cs
+++ My project (1)/Assets/Scripts/Refrigerante/inserir.cs
@@ -1,10 +1,15 @@
+using TMPro;
 using UnityEngine;
+using System.Text;
 using UnityEngine.UI;
 
-public class InserirMoeda : MonoBehaviour
+public class Inserir : MonoBehaviour
 {
     public int moedas = 0;
     private Text moedasText;
+    
+    public TextMeshProUGUI avisoTMP;
+    public GameObject refrigerante;
     
     public void AdicionarMoeda()
     {
@@ -19,5 +24,11 @@ public class InserirMoeda : MonoBehaviour
     {
         moedas--;
         AtualizarMoedas();
+    }
+    public void AdicionarRefrigerante()
+    {
+        avisoTMP.gameObject.SetActive(true);
+        refrigerante.SetActive(true);
+        avisoTMP.text = refrigerante.name;
     }
 }
