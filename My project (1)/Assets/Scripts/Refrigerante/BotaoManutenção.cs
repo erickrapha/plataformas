75e27e95cd4ea36e375c43c419ffd2730523dd59
Erickrapha <erick.r@escolar.ifrn.edu.br>
2025-09-12 14:08:30 -0300

    textoManutencao

--- My project (1)/Assets/Scripts/Refrigerante/BotaoManutenção.cs
+++ My project (1)/Assets/Scripts/Refrigerante/BotaoManutenção.cs
@@ -1,24 +1,39 @@
 using UnityEngine;
+using TMPro;
 
 public class BotaoManutenção : MonoBehaviour
 {
     public Animator animator;
+    public TMP_Text textoManutenção;
     
     private bool emManutencao;
 
     void Start()
     {
+        emManutencao = false;
         animator.SetBool("EmManutencao", false);
+        if (textoManutenção != null)
+        {
+            textoManutenção.gameObject.SetActive(false);
+        }
     }
     public void IniciarManutencao()
     {
         emManutencao = true;
         animator.SetBool("EmManutencao", true);
+        if (textoManutenção != null)
+        {
+            textoManutenção.gameObject.SetActive(true);
+        }
     }
     public void TerminarManutencao()
     {
         emManutencao = false;
         animator.SetBool("EmManutencao", false);
+        if (textoManutenção != null)
+        {
+            textoManutenção.gameObject.SetActive(false);
+        }
     }
     public void AlternarManutencao()
     {
